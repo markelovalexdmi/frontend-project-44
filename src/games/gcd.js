e220cbef7efd13338eb2e2f30bdc86ba08fa86b2
Markelov Alex <markelovalexdmi@gmail.com>
2023-01-23 21:25:50 +0300

    Changes after the third review(lint changes).

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,7 +5,7 @@ const description = 'Find the greatest common divisor of given numbers.';
 
 const calculateGcd = (number1, number2) => {
   if (number2 === 0) {
-      return number1;
+    return number1;
   }
   return calculateGcd(number2, number1 % number2);
 };
